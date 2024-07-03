9591ee6ce0fc9585bc274ef2ba90af97988c8e7c
Ryan Sharkey <ryansharkey@Ryans-Mac-mini.lan>
2024-07-03 08:51:29 -0400

    private

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+// import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-import Navbar from './Navbar'; // Adjust the path as necessary
-import Page1 from './Page1';
-import Page2 from './Page2';
-// Import more pages as needed
+// import Navbar from './Navbar'; // Adjust the path as necessary
+// import Page1 from './Page1';
+// import Page2 from './Page2';
+// // Import more pages as needed
 
-// A wrapper component for handling route transitions
-const AnimatedRoutes = () => {
+// // A wrapper component for handling route transitions
+// const AnimatedRoutes = () => {
 
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <Navbar />
+//     </div>
+//   );
+// };
 
-function App() {
-  return (
-    <Router>
-      <AnimatedRoutes />
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//       <AnimatedRoutes />
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
